6b2c5e5cc22466dee602d4eeed039d27b0ba9fa7
lwygit <jyh120@outlook.com>
2019-10-28 11:47:24 +0800

    可以返回webView的上一页

--- app/src/main/java/com/rmtzx/activitytest/SecondActivity.java
+++ app/src/main/java/com/rmtzx/activitytest/SecondActivity.java
@@ -3,12 +3,12 @@ package com.rmtzx.activitytest;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class SecondActivity extends BaseActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +21,13 @@ public class SecondActivity extends BaseActivity {
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 if (url == null) return false;
 
-                try{
-                    if(!url.startsWith("http://") && !url.startsWith("https://")){
+                try {
+                    if (!url.startsWith("http://") && !url.startsWith("https://")) {
                         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
                         startActivity(intent);
                         return true;
                     }
-                }catch (Exception e){//防止crash (如果手机上没有安装处理某个scheme开头的url的APP, 会导致crash)
+                } catch (Exception e) {//防止crash (如果手机上没有安装处理某个scheme开头的url的APP, 会导致crash)
                     return true;//没有安装该app时，返回true，表示拦截自定义链接，但不跳转，避免弹出上面的错误页面
                 }
 
@@ -40,5 +40,13 @@ public class SecondActivity extends BaseActivity {
         webView.loadUrl("http://www.baidu.com");
     }
 
-
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        WebView webView = findViewById(R.id.web_view);
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack();// 返回前一个页面
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 }
